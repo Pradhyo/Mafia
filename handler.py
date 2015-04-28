5a02c8515b0447926afc479b9c019b36b076b73b
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-28 17:24:40 -0400

    Create user entities when they enter name after joining a room

--- handler.py
+++ handler.py
@@ -36,21 +36,25 @@ class MainHandler(Handler):
 	def post(self):
 		room_name = self.request.get('room_name')
 		room_password = self.request.get('room_password')
-		current_room = self.request.cookies.get('room')
+		
 		if room_password and room_name:
 			temp_room = Room(name = room_name, password = room_password)
 			temp_room.put()
 			self.response.set_cookie('room', room_name, max_age=360, path='/')
 			self.redirect('/#/waiting')		
+		else: 
+			self.redirect('/')
 
+class CreateUser(Handler):
+	def post(self):
+		current_room = self.request.cookies.get('room')
 		if current_room:
 			username = self.request.get('username')
 			if username:
 				temp_user = User(name = username, room = current_room)
 				temp_user.put()
 				self.redirect('/#/nightmafia')
-		else: 
-			self.redirect('/')
+		
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import webapp2
-from handler import MainHandler
+from handler import MainHandler, CreateUser
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+							   ('/newuser', CreateUser)], 
+							   debug=True)
