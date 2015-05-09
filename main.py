574623d12f48c6fba06e9aad2b8c36d994bad21d
Pradhyo Bijja <pradhyo@nyu.edu>
2015-05-09 12:37:12 -0400

    Handlers to handle night and day votes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import webapp2
-from handler import MainHandler, CreateUser, JoinRoom, GamePlay, Proceed
+from handler import MainHandler, CreateUser, JoinRoom, GamePlay, Proceed, NightVotes, DayVotes
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
@@ -10,6 +10,9 @@ app = webapp2.WSGIApplication([('/', MainHandler),
 							   ('/joinroom', JoinRoom),
 							   ('/game', GamePlay),
 							   ('/proceed', Proceed),
+							   ('/nightvotes', NightVotes),
+							   ('/dayvotes', DayVotes),
 							   ('/newuser', CreateUser)], 
 							   debug=True)
+
 
